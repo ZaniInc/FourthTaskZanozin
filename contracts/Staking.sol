4a22846507decdb7ff84737381964640beb7e41e
ZaniInc <stridestoreholding@gmail.com>
2022-07-01 20:33:56 +0300

    "task4"

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -9,19 +9,14 @@ import "../interfaces/IVesting.sol";
 
 contract Staking {
     using SafeERC20 for IERC20;
-    string private greeting;
 
-    constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        greeting = _greeting;
-    }
-
-    function greet() public view returns (string memory) {
-        return greeting;
-    }
-
-    function setGreeting(string memory _greeting) public {
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-        greeting = _greeting;
+    IERC20 public token;
+    
+    constructor(address token_){
+         require(
+            isContract(token_),
+            "Error : Incorrect address , only contract address"
+        );
+        token = IERC20(token_);
     }
 }
