951ff18a98b0213d511999052d90b75ab9957788
ZaniInc <stridestoreholding@gmail.com>
2022-07-20 13:39:19 +0300

    "update"

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -7,38 +7,78 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "../interfaces/IStaking.sol";
 
-contract Staking is Ownable {
+/**
+ * @title Vesting
+ * @author Pavel Zanozin
+ * @notice This SC for allows users to stake their
+ * tokens for passive income
+ */
+contract Staking is Ownable, IStaking {
     using SafeERC20 for IERC20;
+    using Address for address;
 
-    struct Investor {
-        uint256 startDate;
-        uint256 finishDate;
-        uint256 stakingAmount;
-        uint256 userRewardPerTokens; // равняется rewardPerTokensTotal
-        uint256 rewardPaid;
-    }
-    struct StakingParams {
-        uint256 stakingStartDate;
-        uint256 stakingPeriod;
-        uint256 feePercent;
-        uint256 cooldownPeriod;
-    }
-
-    mapping(address => Investor) public investorList;
+    /**
+     * @dev create object of struct
+     * @notice using for set staking params
+     */
     StakingParams public stakingParams;
+
+    /**
+     * @dev contain max value for staking pool
+     * in one time
+     */
     uint256 public constant MAX_STAKING_POOL = 5000000 ether;
-    uint256 public lastTimeUpdate; // last time when someone call staking or unstaking
-    uint256 public apr; //minting tokens per 1 second
+
+    /**
+     * @dev contain 100% in 1e18
+     * @notice using for math
+     */
+    uint256 public constant ONE_HUNDRED_PERCENT = 100 ether;
+
+    /**
+     * @dev contain max reward that can be accumulated
+     * @notice must be equal 10% of 'MAX_STAKING_POOL'
+     */
     uint256 public constant MAX_REWARD = 500_000 ether;
+
+    /**
+     * @dev contain all investors whos stake tokens
+     */
+    mapping(address => Investor) public investorList;
+
+    /**
+     * @dev last time in seconds when someone call
+     * staking or unstaking
+     */
+    uint256 public lastTimeUpdate;
+
+    /**
+     * @dev how many reward tokens minting per 1 second
+     */
+    uint256 public apr;
+
+    /**
+     * @dev contain how many reward tokens accumulated
+     */
     uint256 public rewardPerTokenStored;
+
+    /**
+     * @dev contain how many tokens stake
+     * at the monment
+     */
     uint256 public stakingTotalAmount;
-    uint256 public maxStakingCap;
 
     IERC20 public token;
 
+    /**
+     * @dev Set 'token' IERC20 to interact with thrid party token
+     *
+     * @param token_ - of ERC20 contract
+     * @notice set staking params
+     */
     constructor(address token_) {
         require(
-            Address.isContract(token_),
+            token_.isContract(),
             "Error : Incorrect address , only contract address"
         );
         token = IERC20(token_);
@@ -47,62 +87,170 @@ contract Staking is Ownable {
         stakingParams.cooldownPeriod = 10 days;
     }
 
-    function  updateReward(address caller) internal {
-        rewardPerTokenStored = rewardPerToken();
-        lastTimeUpdate = block.timestamp;
-        investorList[caller].rewardPaid = earned(caller);
-        investorList[caller].userRewardPerTokens = rewardPerTokenStored;
-    }
-
+    /**
+     * @dev run staking period and transfer max reward
+     * value to this contract
+     *
+     * @param start_ - input time when staking period
+     * will be start
+     * @param rewardsAmount_ - max reward on this period
+     * @param apr_ - how many tokens wiil be collect
+     * per second
+     *
+     * @notice function can call only owner of SC
+     */
     function setRewards(
         uint256 start_,
         uint256 rewardsAmount_,
         uint256 apr_
-    ) external onlyOwner {
-        require(rewardsAmount_ <= MAX_REWARD, "1");
-        require(start_ >= block.timestamp, "12");
-        // require(finish_ > start_,"13");
-        require(rewardsAmount_ > 0 && start_ > 0 && apr_ > 0, "31");
+    ) external override onlyOwner {
+        require(
+            rewardsAmount_ <= MAX_REWARD,
+            "Error : reward above limit - 500000"
+        );
+        require(
+            start_ >= block.timestamp,
+            "Error : start time must be greater than current time"
+        );
+        require(
+            rewardsAmount_ > 0 && start_ > 0 && apr_ > 0,
+            "Error : one of params equal to 0"
+        );
         stakingParams.stakingStartDate = start_;
+        stakingParams.stakingFinishDate =
+            stakingParams.stakingStartDate +
+            stakingParams.stakingPeriod;
         apr = apr_;
-        maxStakingCap = rewardsAmount_ * 100 ether / apr;
         token.safeTransferFrom(msg.sender, address(this), rewardsAmount_);
+        emit SetRewards(start_, rewardsAmount_, apr_);
     }
 
-    function stake(uint256 amount_) external {
-        updateReward(msg.sender);
+    /**
+     * @dev function allow user stake tokens
+     *
+     * @param amount_ - how many tokens user want to stake
+     *
+     * @notice function can call by any one , but sum of
+     * all staking tokens can't be higher than 'MAX_STAKING_POOL'
+     */
+    function stake(uint256 amount_) external override {
+        require(
+            block.timestamp >
+                investorList[msg.sender].startDate +
+                    stakingParams.cooldownPeriod ||
+                investorList[msg.sender].startDate < 0,
+            "Error : for re-staking wait 10 days"
+        );
+        require(amount_ > 0, "Error : you can't stake 0 tokens");
+        require(
+            stakingParams.stakingStartDate > 0,
+            "Error : staking has not started yet"
+        );
+        require(
+            stakingParams.stakingStartDate + stakingParams.stakingPeriod >
+                block.timestamp,
+            "Error : staking period has end"
+        );
+        require(stakingTotalAmount <= MAX_STAKING_POOL);
+        _updateReward(msg.sender);
         investorList[msg.sender].stakingAmount += amount_;
         stakingTotalAmount += amount_;
-        investorList[msg.sender].startDate = block.timestamp;
-        token.safeTransferFrom(msg.sender, address(this), amount_);
-        // rewardPerTokensTotal += amount_;
+        if (stakingTotalAmount > MAX_STAKING_POOL) {
+            revert("Error : your stake value too high");
+        }
+        if (investorList[msg.sender].startDate > 0) {
+            token.safeTransferFrom(msg.sender, address(this), amount_);
+            emit Stake(msg.sender, amount_);
+        } else {
+            investorList[msg.sender].startDate = block.timestamp;
+            token.safeTransferFrom(msg.sender, address(this), amount_);
+            emit Stake(msg.sender, amount_);
+        }
     }
 
-    function unStake() external {
-        updateReward(msg.sender);
-        uint256 amount = investorList[msg.sender].stakingAmount + investorList[msg.sender].rewardPaid;
+    /**
+     * @dev function allow user to withdraw sum of stake tokens
+     * by caller + collected reward for caller
+     *
+     * @notice if the user call this function before the end of staking period
+     * he will receive only 60% of his reward
+     */
+    function unStake() external override {
+        require(
+            investorList[msg.sender].stakingAmount > 0,
+            "Error: you are not investor"
+        );
+        _updateReward(msg.sender);
+        uint256 amount;
+        uint256 reward = investorList[msg.sender].reward;
+        if (block.timestamp < stakingParams.stakingFinishDate) {
+            reward = reward - ((reward * 40 ether) / ONE_HUNDRED_PERCENT);
+            amount = investorList[msg.sender].stakingAmount + reward;
+        } else {
+            amount = investorList[msg.sender].stakingAmount + reward;
+        }
         stakingTotalAmount -= investorList[msg.sender].stakingAmount;
-        investorList[msg.sender].startDate = 0;
+        delete investorList[msg.sender].startDate;
         investorList[msg.sender].finishDate = block.timestamp;
-        investorList[msg.sender].stakingAmount = 0;
-        investorList[msg.sender].rewardPaid = 0;
+        delete investorList[msg.sender].stakingAmount;
+        delete investorList[msg.sender].reward;
         token.safeTransfer(msg.sender, amount);
+        emit UnStake(msg.sender, amount);
     }
 
-    function rewardPerToken() internal returns (uint256) {
-        uint256 rr = ((stakingTotalAmount * apr) / 100 ether) / 365 days ;
+    /**
+     * @dev internal function which work when someone
+     * call 'stake' or 'unstake' functions
+     *
+     * @notice Update list :
+     *
+     * Update 'rewardPerTokenStored' to actual value of collected reward
+     * Update 'lastTimeUpdate' - last time when some one stake or unstake
+     * Update 'investorList[caller_].reward' - how many tokens caller
+     * can withdraw like reward
+     * Update 'investorList[caller_].userRewardPerTokens' - how many reward
+     * user collect in last time when he stake or unstake
+     */
+    function _updateReward(address caller_) internal {
+        rewardPerTokenStored = _rewardPerToken();
+        lastTimeUpdate = block.timestamp;
+        investorList[caller_].reward = _earned(caller_);
+        investorList[caller_].userRewardPerTokens = rewardPerTokenStored;
+    }
+
+    /**
+     * @dev calculate how many reward collected by all users
+     */
+    function _rewardPerToken() internal view returns (uint256) {
+        uint256 rewardRate = ((stakingTotalAmount * apr) /
+            ONE_HUNDRED_PERCENT) / 365 days;
         if (stakingTotalAmount == 0) {
             return 0;
-        }
-        else {
-             // return rewardPerTokenStored + ((((block.timestamp - lastTimeUpdate)) * rr));
-            return rewardPerTokenStored + ((rr * (block.timestamp - lastTimeUpdate) * 1e18)) / stakingTotalAmount;
+        } else {
+            return
+                rewardPerTokenStored +
+                ((rewardRate * (block.timestamp - lastTimeUpdate) * 1e18)) /
+                stakingTotalAmount;
         }
     }
 
-    // сколько токенов уже заработал юзер
-    function earned(address caller) internal returns (uint256) {
-        uint256 total = (investorList[caller].stakingAmount * (rewardPerToken() - investorList[caller].userRewardPerTokens)) / 1e18;
-        return total;
+    /**
+     * @dev calculate how many reward collected by each user
+     */
+    function _earned(address caller_) internal view returns (uint256) {
+        if (block.timestamp < stakingParams.stakingFinishDate) {
+            return
+                (investorList[caller_].stakingAmount *
+                    (_rewardPerToken() -
+                        investorList[caller_].userRewardPerTokens)) / 1e18;
+        }
+        if (block.timestamp >= stakingParams.stakingFinishDate) {
+            uint256 rewardRate = ((investorList[caller_].stakingAmount * apr) /
+                ONE_HUNDRED_PERCENT) / 365 days;
+            return
+                rewardRate *
+                (stakingParams.stakingFinishDate -
+                    investorList[caller_].startDate);
+        }
     }
 }
--- interfaces/IStaking.sol
+++ interfaces/IStaking.sol
@@ -2,5 +2,102 @@
 
 pragma solidity 0.8.7;
 
+interface IStaking {
 
-interface IStaking {}
+    /**
+     * @dev Store info about beneficiaries
+     *
+     * @param startDate - when user call 'stake' in first time
+     * @param finishDate - when user call 'unstake' in lastTime
+     * @param stakingAmount - how many tokens user staked
+     * @param userRewardPerTokens - how many reward user accumulated
+     * @param userRewardPerTokens - how many reward user collect
+     */
+    struct Investor {
+        uint256 startDate;
+        uint256 finishDate;
+        uint256 stakingAmount;
+        uint256 userRewardPerTokens;
+        uint256 reward;
+    }
+
+    /**
+     * @dev Store info about beneficiaries
+     *
+     * @param stakingStartDate - when did staking period start
+     * @param stakingFinishDate - when users stop collect reward
+     * @param stakingPeriod - how long staking
+     * @param feePercent - if user un stake before staking is end
+     * he will pay fee from collected reward
+     * @param cooldownPeriod - when user can re-stake
+     */
+    struct StakingParams {
+        uint256 stakingStartDate;
+        uint256 stakingFinishDate;
+        uint256 stakingPeriod;
+        uint256 feePercent;
+        uint256 cooldownPeriod;
+    }
+
+    /**
+     * @dev event logs info about date when staking is start
+     *
+     * @param startTime - time when staking period is started
+     * @param reward - how many rewards can collect users
+     * @param apr - how many tokens collected per second
+     */
+    event SetRewards(uint256 startTime, uint256 reward, uint256 apr);
+
+    /**
+     * @dev event logs info about stake by user
+     *
+     * @param investor - whos staking
+     * @param amount - how many tokens stake by user
+     */
+    event Stake(address investor, uint256 amount);
+
+    /**
+     * @dev event logs info about stake by user
+     *
+     * @param investor - whos un staking
+     * @param amount - how many tokens un stake by user
+     */
+    event UnStake(address investor, uint256 amount);
+    
+    /**
+     * @dev run staking period and transfer max reward
+     * value to this contract
+     *
+     * @param start_ - input time when staking period
+     * will be start
+     * @param rewardsAmount_ - max reward on this period
+     * @param apr_ - how many tokens wiil be collect
+     * per second
+     *
+     * @notice function can call only owner of SC
+     */
+    function setRewards(
+        uint256 start_,
+        uint256 rewardsAmount_,
+        uint256 apr_
+    ) external;
+
+    /**
+     * @dev function allow user stake tokens
+     *
+     * @param amount_ - how many tokens user want to stake
+     *
+     * @notice function can call by any one , but sum of
+     * all staking tokens can't be higher than 'MAX_STAKING_POOL'
+     */
+    function stake(uint256 amount_) external;
+
+    /**
+     * @dev function allow user to withdraw sum of stake tokens
+     * by caller + collected reward for caller
+     *
+     * @notice if the user call this function before the end of staking period
+     * he will receive only 60% of his reward
+     */
+    function unStake() external;
+}
